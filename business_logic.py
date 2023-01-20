1db60b90fa2bd487e724588e6c78ab57f1c23a4b
yoni kremer <yoni.kremer@gmail.com>
2023-01-20 16:13:18 +0200

    better edge case handling

--- business_logic.py
+++ business_logic.py
@@ -1,3 +1,6 @@
+from numbers import Real
+from typing import Callable
+
 from constants import (
     MASS_NO_CHARGE_KG,
     MAX_TAKEOFF_TIME_SEC,
@@ -12,41 +15,65 @@ maximal_total_mass = ENGINES_FORCE_NEWTON / minimal_acceleration
 maximal_charge_mass = maximal_total_mass - MASS_NO_CHARGE_KG
 
 
-def calculate_acceleration(charge_mass_kg):
+class ChargeMassErrorTooBig(ValueError):
+    pass
+
+
+def is_valid_charge_mass(charge_mass_kg) -> bool:
+    """Check if the charge mass is valid."""
+    return isinstance(charge_mass_kg, Real) and charge_mass_kg >= 0
+
+
+def is_valid_charge_mass_string(charge_mass_kg_str: str) -> bool:
+    """Check if the charge mass is valid."""
+    try:
+        charge_mass_kg = float(charge_mass_kg_str)
+    except ValueError:
+        return False
+    return is_valid_charge_mass(charge_mass_kg)
+
+
+def check_charge_mass_input(func: Callable):
+    """Check if the charge mass is valid."""
+    def wrapper(*args, **kwargs):
+        charge_mass_kg = kwargs["charge_mass_kg"] if "charge_mass_kg" in kwargs else args[0]
+        if not is_valid_charge_mass(charge_mass_kg):
+            raise ValueError("The charge mass must be a non-negative number.")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@check_charge_mass_input
+def calculate_acceleration(charge_mass_kg: Real) -> Real:
     """Calculate the acceleration of the plane with a given charge mass."""
-    if charge_mass_kg < 0:
-        raise ValueError("The charge mass must non negative.")
     total_mass_kg = charge_mass_kg + MASS_NO_CHARGE_KG
     return ENGINES_FORCE_NEWTON / total_mass_kg
 
 
-def calculate_takeoff_time(charge_mass_kg):
+@check_charge_mass_input
+def calculate_takeoff_time(charge_mass_kg: Real) -> Real:
     """Calculate the time it takes to take off with a given charge mass.
     raises ValueError if the plane can't take off in MAX_TAKEOFF_TIME_SEC."""
-    if charge_mass_kg < 0:
-        raise ValueError("The charge mass must non negative.")
     acceleration_mps2 = calculate_acceleration(charge_mass_kg)
     takeoff_time_sec = TAKE_OFF_VELOCITY_MPS / acceleration_mps2
     if takeoff_time_sec > MAX_TAKEOFF_TIME_SEC:
-        raise ValueError("The plane will not take off in time.")
+        raise ChargeMassErrorTooBig("The plane will not take off in time.")
     return takeoff_time_sec
 
 
-def calculate_takeoff_distance(charge_mass_kg):
+@check_charge_mass_input
+def calculate_takeoff_distance(charge_mass_kg: Real) -> Real:
     """Calculate the distance it takes to take off with a given charge mass.
     raises ValueError if the plane can't take off in MAX_TAKEOFF_TIME_SEC."""
-    if charge_mass_kg < 0:
-        raise ValueError("The charge mass must non negative.")
     acceleration_mps2 = calculate_acceleration(charge_mass_kg)
     takeoff_time_sec = calculate_takeoff_time(charge_mass_kg)
     return (VELOCITY_0_MPS * takeoff_time_sec) + (acceleration_mps2 * takeoff_time_sec ** 2) / 2
 
 
-def calculate_mass_to_destroy(charge_mass_kg):
+@check_charge_mass_input
+def calculate_mass_to_destroy(charge_mass_kg: Real) -> Real:
     """Calculate the mass of the charge that will destroy the plane.
     raises ValueError if the plane can't take off in MAX_TAKEOFF_TIME_SEC."""
-    if charge_mass_kg < 0:
-        raise ValueError("The charge mass must non negative.")
     if charge_mass_kg <= maximal_charge_mass:
         # you don't need to destroy any charge
         return 0
@@ -54,17 +81,19 @@ def calculate_mass_to_destroy(charge_mass_kg):
 
 
 def main():
-    charge_mass_kg = float(input("Enter the charge mass in kg: "))
-    while charge_mass_kg < 0:
+    charge_mass_kg_str = input("Enter the charge mass in kg: ")
+    while not is_valid_charge_mass_string(charge_mass_kg_str):
         print("The charge mass must be positive.")
-        charge_mass_kg = float(input("Enter the charge mass in kg: "))
+        charge_mass_kg_str = input("Enter the charge mass in kg: ")
+
+    charge_mass_kg = float(charge_mass_kg_str)
 
     try:
         takeoff_time_sec = calculate_takeoff_time(charge_mass_kg)
         print(f"The plane will take off in {takeoff_time_sec} seconds.")
         takeoff_distance_m = calculate_takeoff_distance(charge_mass_kg)
         print(f"The plane will take off {takeoff_distance_m} meters.")
-    except ValueError:
+    except ChargeMassErrorTooBig:
         print("The plane will not take off in time.")
         print(f"In order to take off in time, you need to destroy"
               f" {calculate_mass_to_destroy(charge_mass_kg)} kg of charge.")
--- tests/test_buisness_logic.py
+++ tests/test_buisness_logic.py
@@ -5,6 +5,22 @@ import business_logic
 
 
 class MyTestCase(unittest.TestCase):
+    def test_is_valid_charge_mass(self):
+        self.assertTrue(business_logic.is_valid_charge_mass(0))
+        self.assertTrue(business_logic.is_valid_charge_mass(business_logic.maximal_charge_mass))
+        self.assertTrue(business_logic.is_valid_charge_mass(1.11))
+        self.assertFalse(business_logic.is_valid_charge_mass(-1))
+        self.assertFalse(business_logic.is_valid_charge_mass("wbudwbwdb"))
+        self.assertRaises(TypeError, business_logic.is_valid_charge_mass)
+
+    def test_is_valid_charge_mass_string(self):
+        self.assertTrue(business_logic.is_valid_charge_mass_string("0"))
+        self.assertTrue(business_logic.is_valid_charge_mass_string("1.11"))
+        self.assertFalse(business_logic.is_valid_charge_mass_string("-1"))
+        # noinspection SpellCheckingInspection
+        self.assertFalse(business_logic.is_valid_charge_mass_string("wbudwbwdb"))
+        self.assertRaises(TypeError, business_logic.is_valid_charge_mass)
+
     def test_calculate_acceleration(self):
         self.assertEqual(business_logic.calculate_acceleration(0), ENGINES_FORCE_NEWTON / MASS_NO_CHARGE_KG)
         self.assertRaises(ValueError, business_logic.calculate_acceleration, -1)
@@ -20,7 +36,7 @@ class MyTestCase(unittest.TestCase):
         take_off_time_zero_mass = business_logic.calculate_takeoff_time(0)
         self.assertEqual(business_logic.calculate_takeoff_distance(0),
                          (VELOCITY_0_MPS * take_off_time_zero_mass) + (
-                                     acceleration_zero_mass * take_off_time_zero_mass ** 2) / 2)
+                                 acceleration_zero_mass * take_off_time_zero_mass ** 2) / 2)
         self.assertRaises(ValueError, business_logic.calculate_takeoff_distance, -1)
         self.assertRaises(ValueError, business_logic.calculate_takeoff_distance, 1000000)
 
