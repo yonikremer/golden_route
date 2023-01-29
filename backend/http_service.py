6bb7a3071eb24806c74de5c7b534288c6481fca6
yoni kremer <yoni.kremer@gmail.com>
2023-01-29 14:27:03 +0200

    Small bug fixes

--- backend/http_service.py
+++ backend/http_service.py
@@ -1,11 +1,16 @@
 """an HTTP server that allows you to use the pysics calculator in the business_logic.py file"""
-from numbers import Real
+from typing import Union
 
 import fastapi
 from fastapi.middleware.cors import CORSMiddleware
 
-from business_logic import calculate_acceleration, calculate_takeoff_distance, calculate_takeoff_time, \
-    ChargeMassErrorTooBig, calculate_mass_to_destroy
+from backend.business_logic import (
+    calculate_acceleration,
+    calculate_takeoff_distance,
+    calculate_takeoff_time,
+    ChargeMassErrorTooBig,
+    calculate_mass_to_destroy
+)
 
 app = fastapi.FastAPI()
 
@@ -17,10 +22,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+NumberType = Union[int, float]
+
 
 @app.get("/acceleration")
 @app.get("/acceleration/{charge_mass_kg}")
-def acceleration(charge_mass_kg: Real):
+def acceleration(charge_mass_kg: NumberType):
     """Calculate the acceleration of the plane with a given charge mass.
     Raises:
         InvalidChargeMass: if the charge mass is not a non-negative number."""
@@ -32,7 +39,7 @@ def acceleration(charge_mass_kg: Real):
 
 @app.get("/takeoff_time")
 @app.get("/takeoff_time/{charge_mass_kg}")
-def takeoff_time(charge_mass_kg: Real):
+def takeoff_time(charge_mass_kg: NumberType):
     """Gets the mass of the charge in kilograms (non-negative number).
     Calculate the time it takes to take off with the charge mass.
     Raises:
@@ -48,7 +55,7 @@ def takeoff_time(charge_mass_kg: Real):
 
 @app.get("/takeoff_distance")
 @app.get("/takeoff_distance/{charge_mass_kg}")
-def takeoff_distance(charge_mass_kg: Real):
+def takeoff_distance(charge_mass_kg: NumberType):
     """Gets the mass of the charge in kilograms (non-negative number).
         Calculate the distance it takes to take off with a given charge mass.
         raises ValueError if the plane can't take off in MAX_TAKEOFF_TIME_SEC.
@@ -64,7 +71,7 @@ def takeoff_distance(charge_mass_kg: Real):
 
 @app.get("/mass_to_destroy")
 @app.get("/mass_to_destroy/{charge_mass_kg}")
-def mass_to_destroy(charge_mass_kg: Real):
+def mass_to_destroy(charge_mass_kg: NumberType):
     """Gets the mass of the charge in kilograms (non-negative number).
     If the plane can take off in time,
     You don't need to destroy any charge, so the function returns 0.
