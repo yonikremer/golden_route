0a5cbe23ee932b31b71c13830b41fdac6a260a3f
yoni kremer <yoni.kremer@gmail.com>
2023-01-29 12:14:46 +0200

    deleted unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Weather from "./Weather";
