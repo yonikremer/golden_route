255254030a6a08edf2eb54ec8e9f96f4eb164af2
yoni kremer <yoni.kremer@gmail.com>
2023-01-29 13:50:52 +0200

    replaced unittest with pytest

--- tests/test_buisness_logic.py
+++ tests/test_buisness_logic.py
@@ -1,50 +1,102 @@
-import unittest
+import sys
+from os.path import dirname, abspath
 
-from constants import MASS_NO_CHARGE_KG, TAKE_OFF_VELOCITY_MPS, ENGINES_FORCE_NEWTON, VELOCITY_0_MPS
-import business_logic
+from pytest import approx, main, raises
+
+test_directory = dirname(abspath(__file__))
+project_directory = dirname(test_directory)
+sys.path.append(project_directory)
+
+from backend.constants import (
+    MASS_NO_CHARGE_KG,
+    MAX_TAKEOFF_TIME_SEC,
+    TAKE_OFF_VELOCITY_MPS,
+    ENGINES_FORCE_NEWTON,
+    VELOCITY_0_MPS,
+)
+
+from backend.business_logic import (
+    is_valid_charge_mass,
+    calculate_acceleration,
+    is_valid_charge_mass_string,
+    minimal_acceleration,
+    maximal_total_mass,
+    maximal_charge_mass,
+    calculate_takeoff_time,
+    calculate_takeoff_distance,
+    calculate_mass_to_destroy, ChargeMassErrorTooBig, InvalidChargeMass,
+)
 
 
-class MyTestCase(unittest.TestCase):
-    def test_is_valid_charge_mass(self):
-        self.assertTrue(business_logic.is_valid_charge_mass(0))
-        self.assertTrue(business_logic.is_valid_charge_mass(business_logic.maximal_charge_mass))
-        self.assertTrue(business_logic.is_valid_charge_mass(1.11))
-        self.assertFalse(business_logic.is_valid_charge_mass(-1))
-        self.assertFalse(business_logic.is_valid_charge_mass("wbudwbwdb"))
-        self.assertRaises(TypeError, business_logic.is_valid_charge_mass)
+def test_minimal_acceleration():
+    assert minimal_acceleration == TAKE_OFF_VELOCITY_MPS / MAX_TAKEOFF_TIME_SEC
 
-    def test_is_valid_charge_mass_string(self):
-        self.assertTrue(business_logic.is_valid_charge_mass_string("0"))
-        self.assertTrue(business_logic.is_valid_charge_mass_string("1.11"))
-        self.assertFalse(business_logic.is_valid_charge_mass_string("-1"))
-        # noinspection SpellCheckingInspection
-        self.assertFalse(business_logic.is_valid_charge_mass_string("wbudwbwdb"))
-        self.assertRaises(TypeError, business_logic.is_valid_charge_mass)
 
-    def test_calculate_acceleration(self):
-        self.assertEqual(business_logic.calculate_acceleration(0), ENGINES_FORCE_NEWTON / MASS_NO_CHARGE_KG)
-        self.assertRaises(ValueError, business_logic.calculate_acceleration, -1)
+def test_maximal_total_mass():
+    assert maximal_total_mass == ENGINES_FORCE_NEWTON / minimal_acceleration
 
-    def test_calculate_takeoff_time(self):
-        self.assertEqual(business_logic.calculate_takeoff_time(0),
-                         TAKE_OFF_VELOCITY_MPS / (ENGINES_FORCE_NEWTON / MASS_NO_CHARGE_KG))
-        self.assertRaises(ValueError, business_logic.calculate_takeoff_time, -1)
-        self.assertRaises(ValueError, business_logic.calculate_takeoff_time, 1000000)
 
-    def test_calculate_takeoff_distance(self):
-        acceleration_zero_mass = business_logic.calculate_acceleration(0)
-        take_off_time_zero_mass = business_logic.calculate_takeoff_time(0)
-        self.assertEqual(business_logic.calculate_takeoff_distance(0),
-                         (VELOCITY_0_MPS * take_off_time_zero_mass) + (
-                                 acceleration_zero_mass * take_off_time_zero_mass ** 2) / 2)
-        self.assertRaises(ValueError, business_logic.calculate_takeoff_distance, -1)
-        self.assertRaises(ValueError, business_logic.calculate_takeoff_distance, 1000000)
+def test_maximal_charge_mass():
+    assert maximal_charge_mass == maximal_total_mass - MASS_NO_CHARGE_KG
 
-    def test_calculate_mass_to_destroy(self):
-        self.assertEqual(business_logic.calculate_mass_to_destroy(0), 0)
-        self.assertRaises(ValueError, business_logic.calculate_mass_to_destroy, -1)
-        self.assertEqual(business_logic.calculate_mass_to_destroy(business_logic.maximal_charge_mass + 1), 1)
+
+def test_is_valid_charge_mass():
+    assert is_valid_charge_mass(0)
+    assert is_valid_charge_mass(maximal_charge_mass)
+    assert is_valid_charge_mass(1.11)
+    assert not is_valid_charge_mass(-1)
+    assert not is_valid_charge_mass("wbudwbwdb")
+    with raises(TypeError):
+        is_valid_charge_mass()
+
+
+def test_is_valid_charge_mass_string():
+    assert is_valid_charge_mass_string("0")
+    assert is_valid_charge_mass_string("1.11")
+    assert not is_valid_charge_mass_string("-1")
+    # noinspection SpellCheckingInspection
+    assert not is_valid_charge_mass_string("wbudwbwdb")
+    with raises(TypeError):
+        is_valid_charge_mass_string()
+
+
+def test_calculate_acceleration():
+    assert calculate_acceleration(0) == ENGINES_FORCE_NEWTON / MASS_NO_CHARGE_KG
+    assert calculate_acceleration(maximal_charge_mass) == minimal_acceleration
+    assert calculate_acceleration(1.11) == ENGINES_FORCE_NEWTON / (MASS_NO_CHARGE_KG + 1.11)
+    with raises(InvalidChargeMass):
+        calculate_acceleration(-1)
+    with raises(ChargeMassErrorTooBig):
+        calculate_acceleration(maximal_charge_mass + 1)
+
+
+def test_calculate_takeoff_time():
+    assert calculate_takeoff_time(0) == TAKE_OFF_VELOCITY_MPS / (ENGINES_FORCE_NEWTON / MASS_NO_CHARGE_KG)
+    assert calculate_takeoff_time(maximal_charge_mass) == approx(MAX_TAKEOFF_TIME_SEC)
+    with raises(InvalidChargeMass):
+        calculate_takeoff_time(-1)
+    with raises(ChargeMassErrorTooBig):
+        calculate_takeoff_time(maximal_charge_mass + 1)
+
+
+def test_calculate_takeoff_distance():
+    acceleration_zero_mass = calculate_acceleration(0)
+    take_off_time_zero_mass = calculate_takeoff_time(0)
+    assert (calculate_takeoff_distance(0), (VELOCITY_0_MPS * take_off_time_zero_mass) + (
+            acceleration_zero_mass * take_off_time_zero_mass ** 2) / 2)
+    with raises(InvalidChargeMass):
+        calculate_takeoff_distance(-1)
+    with raises(ChargeMassErrorTooBig):
+        calculate_takeoff_distance(maximal_charge_mass + 1)
+
+
+def test_calculate_mass_to_destroy():
+    assert calculate_mass_to_destroy(0) == 0
+    with raises(InvalidChargeMass):
+        calculate_mass_to_destroy(-1)
+    assert calculate_mass_to_destroy(maximal_charge_mass) == 0
+    assert calculate_mass_to_destroy(maximal_charge_mass + 1) == 1
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
